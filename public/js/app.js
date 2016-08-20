956260937dc7a2bb9465b95a2c973c864511f72a
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-08-19 20:08:33 -0400

    initial commit with basic components

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+(function(){
+  angular
+  .module("PLACEHOLDER", [
+    "ui.router",
+    "firebase",
+    "photos"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("photoIndex", {
+      url: "/photos",
+      templateUrl: "js/photos/index.html",
+      controller: "PhotoIndexController",
+      controllerAs: "PhotoIndexViewModel"
+    })
+    .state("photoShow", {
+      url: "/photos/:id",
+      templateUrl: "js/photos/show.html",
+      controller: "PhotoShowController",
+      controllerAs: "PhotoShowViewModel"
+    });
+  }
+}());
