8d215d10eb079a27d33f68454f9710fd2b7a88b7
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-08-22 21:20:12 -0400

    update, delete, and add functionality working for individual photos

--- public/js/albums/show.controller.js
+++ public/js/albums/show.controller.js
@@ -14,15 +14,25 @@
     var vm = this;
     var ref = firebase.database().ref().child("albums/" + $stateParams.id);
 
-    $firebaseObject(ref).$loaded().then(function(album){
+    var syncObject = $firebaseObject(ref);
+    syncObject.$loaded().then(function(album){
       vm.album = album
     });
+
+    vm.create = function(){
+      debugger
+      vm.album.photos.push(vm.newPhoto)
+      vm.album.$save().then(function(){
+        vm.newPhoto = {};
+      });
+    }
 
     vm.update = function(){
       vm.album.$save()
     }
-    vm.delete = function(){
-      vm.album.$remove({id: $stateParams.id})
+    vm.delete = function($index){
+      vm.album.photos.splice($index, 1)
+      vm.album.$save()
     }
   }
 
