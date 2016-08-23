f0d8f3c26b752e346080650644743a7cebcb809a
Chase <chasefreeman13@gmail.com>
2016-08-23 13:11:22 -0400

    map, no functionality, but NO ERRORS

--- public/js/albums/index.controller.js
+++ public/js/albums/index.controller.js
@@ -6,11 +6,11 @@
   .controller("AlbumIndexController", [
     "$firebaseArray",
     "$scope",
-    // "$firebaseObject",
+    "uiGmapGoogleMapApi",
     AlbumIndexControllerFunction
   ]);
 
-  function AlbumIndexControllerFunction($firebaseArray, $scope){
+  function AlbumIndexControllerFunction($firebaseArray, $scope, uiGmapGoogleMapApi){
     var vm = this;
     var ref = firebase.database().ref().child("albums");
     vm.albums = $firebaseArray(ref);
@@ -26,12 +26,46 @@
         $state.go("showIndex", {}, {reload: true});
       })
     }
-    $scope.map = {center: {
-      latitude: 18,
-      longitude: -30},
-      zoom: 3,
-    }
+    $scope.map = {
+    center: {
+        latitude: 38.2,
+        longitude: -98.5795
+    },
+    zoom: 4
+};
+var mapPins = firebase.database().ref();
+$scope.userInput = '';
 
+$scope.markers = $firebaseArray(mapPins);
+
+
+$scope.events = {
+    click: function (map, eventName, handlerArgs) {
+        if ($scope.userInput == '' || $scope.userInput == undefined) {
+                console.log($scope.userInput)
+                return alert("Please fill out a form before placing a marker");
+            }
+        $scope.$apply(function() {
+            //console.log($scope.markers)
+            //console.log(handlerArgs)
+
+            $scope.markers.$add({
+                id: $scope.markers.length,
+                latitude: handlerArgs[0].latLng.lat(),
+                longitude: handlerArgs[0].latLng.lng(),
+                showWindow: true,
+                title: $scope.userInput,
+                options: {
+                    animation: api.Animation.DROP,
+                    title: handlerArgs[0].latLng.toUrlValue(),
+                    disableAutoPan: true
+                }
+            });
+           return $scope.userInput = '';
+        });
     }
+};
+
+};
 
 }());
